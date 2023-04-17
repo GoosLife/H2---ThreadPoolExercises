d7562448cbee6663dec1fcffda88b0dd5d057aa1
Frederik H. Sørensen <72657308+GoosLife@users.noreply.github.com>
2023-04-17 15:49:18 +0200

    Add project files.

---
+++ ThreadPoolExercises/Exercise0.cs
@@ -0,0 +1,39 @@
+﻿/*
+* C# Program to Create Thread Pools
+*/
+using System;
+using System.Threading;
+class ThreadPoolDemo
+{
+    // Each method outputs 3 times
+    public void task1(object obj)
+    {
+        for (int i = 0; i <= 2; i++)
+        {
+            Console.WriteLine("Task 1 is being executed");
+        }
+    }
+    public void task2(object obj)
+    {
+        for (int i = 0; i <= 2; i++)
+        {
+            Console.WriteLine("Task 2 is being executed");
+        }
+    }
+
+    static void MainIgnore()
+    {
+        ThreadPoolDemo tpd = new ThreadPoolDemo();
+
+        for (int i = 0; i < 2; i++) // Create 4 tasks total, 2 at a time.
+                                    // These tasks are executed in an order determined by the CPU at runtime based on available resources.
+        {
+            ThreadPool.QueueUserWorkItem(new WaitCallback(tpd.task1)); // (new WaitCallback(tpd.task1)) and simply (tpd.task1) are equivalent
+                                                                       // and compile to the same byte code.
+
+            ThreadPool.QueueUserWorkItem(tpd.task2);
+        }
+
+        Console.Read();
+    }
+}
---
+++ ThreadPoolExercises/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace ThreadPoolExercises
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch myWatch = new Stopwatch();
+            Console.WriteLine("Thread Pool Execution");
+        }
+
+        static void Process()
+        {
+        }
+
+        static void ProcessWithThreadMethod()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Thread obj = new Thread(Process);
+                obj.Start();
+            }
+        }
+
+        static void ProcessWithThreadPoolMethod()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(Process);
+            }
+        }
+    }
+}
